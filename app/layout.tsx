6bb7b7161bdc3832c97ff34f2a80f6e964983639
LeeSooBin <romaydev@naver.com>
2025-11-09 16:02:40 +0900

    layout.tsx 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,20 @@
-// app/layout.jsx
+// app/layout.tsx
 import Script from "next/script";
 import "./globals.css";
 import { ThemeProvider } from "./dark-mode/ThemeContext";
+import { ReactNode } from "react";
 
 export const metadata = {
   title: "RipMeme",
   description: "유행지난 밈 저장소",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
       <head>
         {/* Google AdSense 스크립트 */}
-        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4844671075935249" crossOrigin="anonymous" strategy="afterInteractive" />
+        <Script async strategy="afterInteractive" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4844671075935249" crossOrigin="anonymous" />
       </head>
       <body>
         <ThemeProvider>{children}</ThemeProvider>
